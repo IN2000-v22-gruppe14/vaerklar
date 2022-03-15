464226e7d76dd867f20fc5d52f31b160eda663d0
LBlend <lblend@protonmail.com>
2022-03-15 04:36:31 +0100

    lag initielle dataklasser for API-respons

---
+++ app/src/main/java/com/example/vaerklar/WeatherData.kt
@@ -0,0 +1,85 @@
+package com.example.vaerklar
+
+data class WeatherData(
+    val geometry: Geometry,
+    val properties: Properties,
+    val type: String
+)
+
+data class Geometry(
+    val coordinates: List<Double>,
+    val type: String
+)
+
+data class Properties(
+    val meta: Meta,
+    val timeseries: List<Timeseries>
+)
+
+data class Meta(
+    val units: Units,
+    val updated_at: String
+)
+
+data class Timeseries(
+    val `data`: Data,
+    val time: String
+)
+
+data class Units(
+    val air_pressure_at_sea_level: String,
+    val air_temperature: String,
+    val air_temperature_max: String,
+    val air_temperature_min: String,
+    val cloud_area_fraction: String,
+    val cloud_area_fraction_high: String,
+    val cloud_area_fraction_low: String,
+    val cloud_area_fraction_medium: String,
+    val dew_point_temperature: String,
+    val fog_area_fraction: String,
+    val precipitation_amount: String,
+    val precipitation_amount_max: String,
+    val precipitation_amount_min: String,
+    val probability_of_precipitation: String,
+    val probability_of_thunder: String,
+    val relative_humidity: String,
+    val ultraviolet_index_clear_sky_max: String,
+    val wind_from_direction: String,
+    val wind_speed: String,
+    val wind_speed_of_gust: String
+)
+
+data class Data(
+    val instant: Instant,
+    val next_12_hours: NextHours,
+    val next_1_hours: NextHours,
+    val next_6_hours: NextHours
+)
+
+data class Instant(
+    val details: Details
+)
+
+data class NextHours(
+    val details: Details,
+    val summary: Summary
+)
+
+data class Details(
+    val air_pressure_at_sea_level: Double,
+    val air_temperature: Double,
+    val cloud_area_fraction: Double,
+    val cloud_area_fraction_high: Double,
+    val cloud_area_fraction_low: Double,
+    val cloud_area_fraction_medium: Double,
+    val dew_point_temperature: Double,
+    val fog_area_fraction: Double,
+    val relative_humidity: Double,
+    val wind_from_direction: Double,
+    val wind_speed: Double,
+    val wind_speed_of_gust: Double
+)
+
+data class Summary(
+    val symbol_code: String
+)

e96dfc42f7eb49df49de734b029cc70251cd41fb
centauridesigns <souhailz@uio.no>
2022-03-22 19:49:00 +0100

    Oppsett for hovedsidens skjelett
    Satt opp skjelett for hovedsiden og opprettet tom ViewModel.

---
+++ app/src/main/java/com/example/vaerklar/MainActivityViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.vaerklar
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+    private val ds = DataSource()
+
+    // @TODO: Get instant weather data.
+    // @TODO: Get location data.
+}

c303a01d07959b55b607a57a7b1a37c90beb828c
potetmos1 <petterssateroy@gmail.com>
2022-04-15 22:00:11 +0200

    added visual part of settings page

---
+++ app/src/main/java/com/example/vaerklar/ui/screens/SettingsScreen.kt
@@ -0,0 +1,67 @@
+package com.example.vaerklar.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import com.example.vaerklar.ui.theme.GradientClearDay
+import kotlin.math.roundToInt
+
+@Composable
+fun SettingsScreen() {
+    Scaffold() {
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = GradientClearDay
+                    )
+                )
+        ) {
+            Column {
+                SettingsTile()
+            }
+        }
+    }
+}
+
+fun toggleTheme(darkmode: Boolean) {
+    TODO("Toggle between light and dark theme")
+}
+
+fun notifications(notify: Boolean) {
+    TODO("Toggles notifications onn and off")
+}
+
+fun performanceMode(performance: Boolean) {
+    TODO("Toggles performance mode, saving battery")
+}
+
+fun adjustWarmth(hotness: Float) {
+    val hot: Int = hotness.roundToInt()
+    TODO("Make this adjust the recommended clothes")
+}
+
+fun clearData() {
+    TODO("Clears all saved data the app has on you")
+}
+
+fun termsAndService() {
+    TODO("Shows a page with terms of Service")
+}
+
+fun invite() {
+    TODO("Invite friends to download the app")
+}
+
+fun helpAndSupport() {
+    TODO("Sends the user to a help page")
+}

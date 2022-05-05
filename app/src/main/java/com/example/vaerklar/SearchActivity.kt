a9c4d8babe8ffede48bf2e939b1fd51ad563cb27
LBlend <lblend@protonmail.com>
2022-05-06 01:55:29 +0200

    fjern ubrukt import

--- app/src/main/java/com/example/vaerklar/SearchActivity.kt
+++ app/src/main/java/com/example/vaerklar/SearchActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.app.ActivityCompat
-import com.example.vaerklar.databinding.ActivityMainBinding
 import com.example.vaerklar.ui.screens.SearchScreen
 
 

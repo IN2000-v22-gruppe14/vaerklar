b3c632741f88944d774caba71ea25a385b08a713
LBlend <lblend@protonmail.com>
2022-05-18 16:06:24 +0200

    legg til termsacitivty

--- app/src/main/java/com/example/vaerklar/TermsActivity.kt
+++ app/src/main/java/com/example/vaerklar/TermsActivity.kt
@@ -1,4 +1,17 @@
 package com.example.vaerklar
 
-class TermsActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.app.ActivityCompat
+import com.example.vaerklar.ui.screens.ToSScreen
+
+class TermsActivity : ComponentActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            ToSScreen()
+        }
+    }
 }

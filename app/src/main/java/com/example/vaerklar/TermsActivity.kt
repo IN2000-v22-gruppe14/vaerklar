94ecd0c2f7c20e962de2f171b8925ebb6202d7ec
LBlend <lblend@protonmail.com>
2022-05-18 19:34:34 +0200

    gjem titlebar

--- app/src/main/java/com/example/vaerklar/TermsActivity.kt
+++ app/src/main/java/com/example/vaerklar/TermsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.vaerklar
 
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.app.ActivityCompat
@@ -9,6 +10,10 @@ import com.example.vaerklar.ui.screens.ToSScreen
 class TermsActivity : ComponentActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Hide title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        requestWindowFeature(Window.FEATURE_ACTION_BAR)
 
         setContent {
             ToSScreen()

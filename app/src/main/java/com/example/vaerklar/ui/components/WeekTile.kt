6276d5f810ddba0306b6590008cb4d8ff9aeb93e
LBlend <lblend@protonmail.com>
2022-05-18 22:54:42 +0200

    forsøk på å (halvveis) refaktorere weektile

--- app/src/main/java/com/example/vaerklar/ui/components/WeekTile.kt
+++ app/src/main/java/com/example/vaerklar/ui/components/WeekTile.kt
@@ -7,7 +7,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
@@ -20,14 +23,16 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.vaerklar.data.*
+import com.example.vaerklar.data.WeatherData
+import com.example.vaerklar.data.iconTranslation
+import com.example.vaerklar.data.weekDayTranslation
+import com.example.vaerklar.data.weekDayTranslationWhole
 import com.example.vaerklar.ui.screens.altColor
 import com.example.vaerklar.ui.screens.baseColor
 import com.example.vaerklar.ui.theme.DayTile
 import com.example.vaerklar.ui.theme.Rubik
 import java.time.LocalDate
 import java.time.Period
-import java.util.*
 
 private var globalTileCounter = 0
 private val showDialog = mutableStateOf(false)
@@ -37,86 +42,73 @@ val dayList = mutableListOf<Day>()
 @Composable
 private fun PopUpScreen(weatherData: WeatherData?){
     AlertDialog(
-
         backgroundColor = baseColor,
         shape = RoundedCornerShape(15.dp),
         modifier = Modifier.height(620.dp),
-
-
         title = {
             Column(
                 Modifier
                     .fillMaxSize()
                     .size(200.dp, 100.dp)
                     .background(baseColor)
-            ){ Box(
-                Modifier
-                    .fillMaxSize()
-
             ) {
-                val avatar = Avatar()
-                var dName = ""
-                if(dayList[globalDayNumber].dayNameWhole != null){
-                    dName = dayList[globalDayNumber].dayNameWhole.toString()
-                }
-                var date = ""
-                if(dayList[globalDayNumber].date != null){
-                    val day = dayList[globalDayNumber].date.toString().substring(8,10)
-                    val month = dayList[globalDayNumber].date.toString().substring(5,7)
-                    date = "$day.$month"
-                }
-                val piss = avatar.avatarMain(weatherData, dName, dayList[globalDayNumber].timeIndex, 0, date)
-                var theString = ""
-                for(i in piss){
-                    if(i != ""){
-                        theString += i.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() } + "\n"
-                    }
-                }
                 Box(
                     Modifier
-                        .fillMaxSize(),
-                    contentAlignment = Alignment.BottomCenter){
+                        .fillMaxSize()
+                ) {
+                    val avatar = Avatar()
+                    val dayName = if (dayList[globalDayNumber].dayNameWhole != null) dayList[globalDayNumber].dayNameWhole.toString() else ""
 
-                    Text(
-                        text = theString,
-                        fontSize = 20.sp,
-                        color = Color.White,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier
-                            .absolutePadding(40.dp, 50.dp, 50.dp, 0.dp)
-                    )}
-            }
+                    var date = ""
+                    if (dayList[globalDayNumber].date != null) {
+                        val day = dayList[globalDayNumber].date.toString().substring(8,10)
+                        val month = dayList[globalDayNumber].date.toString().substring(5,7)
+                        date = "$day.$month"
+                    }
 
+                    val outfit = avatar.avatarMain(weatherData, dayName, dayList[globalDayNumber].timeIndex, 0, date)
+                    var outfitTextRepresentation = ""
+                    for (clothingItem in outfit) {
+                        if (clothingItem != "") {
+                            outfitTextRepresentation += "${clothingItem.capitalize()}\n"
+                        }
+                    }
+                    Box(
+                        Modifier
+                            .fillMaxSize(),
+                        contentAlignment = Alignment.BottomCenter) {
+                        Text(
+                            text = outfitTextRepresentation,
+                            fontSize = 20.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .absolutePadding(40.dp, 50.dp, 50.dp, 0.dp)
+                        )
+                    }
+                }
             }
             Box(
                 Modifier.fillMaxSize(),
-                contentAlignment = Alignment.TopEnd){
-
-                    Text(
-                        text = " "
-                    )
-
-                    Icon(
-                        imageVector = Icons.Default.Close,
-                        "Exit",
-                        modifier = Modifier
-                            .size(35.dp)
-                            .absolutePadding(0.dp, 0.dp, 5.dp, 0.dp)
-                            .clickable { showDialog.value = false }
-                            .offset(0.dp, 10.dp),
-                        tint = Color.White,
-                    )
+                contentAlignment = Alignment.TopEnd) {
+                Text(
+                    text = " "
+                )
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    "Lukk",
+                    modifier = Modifier
+                        .size(35.dp)
+                        .absolutePadding(0.dp, 0.dp, 5.dp, 0.dp)
+                        .clickable { showDialog.value = false }
+                        .offset(0.dp, 10.dp),
+                    tint = Color.White,
+                )
             }
         },
-        onDismissRequest = {
-
-        },
-
-        buttons = {
-
-        }
+        onDismissRequest = {},
+        buttons = {}
     )
-
 }
 
 @Composable
@@ -130,11 +122,14 @@ fun WeekTileItem(day: Day, backgroundColor: Color) {
             .absolutePadding(7.dp, 0.dp, 7.dp, 0.dp)
             .fillMaxHeight()
             .fillMaxWidth()
-            .clickable(onClick = { showDialog.value = true
-                globalDayNumber = day.dayNumber
-            })
+            .clickable(
+                onClick = {
+                    showDialog.value = true
+                    globalDayNumber = day.dayNumber
+                }
+            )
     ) {
-
+        // Temperature
         Text(
             text = day.airTemp.toString(),
             color = Color.White,
@@ -144,14 +139,13 @@ fun WeekTileItem(day: Day, backgroundColor: Color) {
             modifier = Modifier
                 .absolutePadding(5.dp, 0.dp, 0.dp, 0.dp)
         )
-
+        // Weather icon
         Image(
             painter = painterResource(iconTranslation.getValue(day.icon)),
             contentDescription = "Icon",
             modifier = Modifier
                 .width(51.dp)
         )
-
         // Time
         Text(
             text = day.dayName.toString(),
@@ -166,11 +160,10 @@ fun WeekTileItem(day: Day, backgroundColor: Color) {
 @Composable
 // The secondary tile, responsible for displaying today's weather across 4 timeslots. Requires TodayCycler.
 fun WeekTile(weatherData: WeatherData?) {
-    //val dayList = mutableListOf<Day>()
     val currentDate = LocalDate.now()
     var counter = 0
     var dayDiff = 0
-    if(showDialog.value){
+    if (showDialog.value) {
         PopUpScreen(weatherData)
     }
 
@@ -203,7 +196,7 @@ fun WeekTile(weatherData: WeatherData?) {
         counter++
     }
 
-    val day = remember {dayList}
+    val day = remember { dayList }
 
     Card(
         modifier = Modifier
@@ -213,7 +206,6 @@ fun WeekTile(weatherData: WeatherData?) {
         shape = RoundedCornerShape(15.dp),
         elevation = 0.dp
     ) {
-
         LazyRow(
             verticalAlignment = Alignment.CenterVertically,
             contentPadding = PaddingValues(),
@@ -222,7 +214,6 @@ fun WeekTile(weatherData: WeatherData?) {
         ) {
             items(
                 items = day,
-
                 itemContent = {
                     when {
                         globalTileCounter == 1 -> {
@@ -235,7 +226,6 @@ fun WeekTile(weatherData: WeatherData?) {
                             WeekTileItem(day = it, baseColor)
                         }
                     }
-
                     globalTileCounter++
                 }
             )

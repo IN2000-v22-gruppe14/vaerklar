6af13505124f64518aa64116b78dfbd674a997dc
LBlend <lblend@protonmail.com>
2022-05-18 21:02:15 +0200

    gi bedre navn til bildefiler for onboarding

--- app/src/main/java/com/example/vaerklar/data/Page.kt
+++ app/src/main/java/com/example/vaerklar/data/Page.kt
@@ -8,18 +8,18 @@ data class Page(val title: String, val description: String, @DrawableRes val ima
 val onboardPages = listOf(
     Page("Anbefalt for deg",
         "På avataren får du anbefalt klesplagg til været akkurat nå. \n\n Du kan også få en tesktlig beskrivelse ved å trykke på pilen.",
-        R.drawable.bilde1
+        R.drawable.onboard_overview
     ),
     Page("Været nå",
         "Her kan du se temperaturen, været, nedbørsmengde og vindstyrke akkurat nå. \n\n ",
-        R.drawable.bilde3
+        R.drawable.onboard_main_tile
     ),
     Page("Dagens vær",
         "Her kan du se værmeldingen for dagen og trykke på tidspunktene for å få anbefalte klesplagg. \n\n",
-        R.drawable.bilde4
+        R.drawable.onboard_today_tile
     ),
     Page("Ukens vær",
         "Her kan du se værmeldingen for uka og trykke på dagene for å få anbefalt klesplagg. \n\n",
-        R.drawable.bilde5
+        R.drawable.onboard_week_tile
     )
 )

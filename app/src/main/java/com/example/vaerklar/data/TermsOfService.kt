041f104e0cb1101781d88d0aaa93be20ed83c1c9
murvold <madsmurvold@gmail.com>
2022-05-19 11:28:51 +0200

    skrivefeil i tos

--- app/src/main/java/com/example/vaerklar/data/TermsOfService.kt
+++ app/src/main/java/com/example/vaerklar/data/TermsOfService.kt
@@ -10,7 +10,7 @@ class TermsOfService {
                 "Henrik Shi Yu Henriksen\n" +
                 "Mads Severin Murvold\n\n" +
                 "Appen er åpen for alle å bruke og ta inspirasjon fra, men direkte tyveri av " +
-                "koden vil ansees som tyveri av intelektuel eiendom.\n\n" +
+                "koden vil ansees som tyveri av intelektuell eiendom.\n\n" +
                 "Ønskes det å benytte noe fra prosjektet til undervisning kan dette fritt" +
                 "gjøres, men det ønskelig at skaperne blir opplyst om dette. "
 
